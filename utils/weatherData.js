4dd8c0c86289ff378ff3e40c7cf08e9b00e99954
RAJHIMANSHUPATEL <blazehimanshuraj@gmail.com>
2024-04-04 04:57:37 +0530

    added dotenv

--- utils/weatherData.js
+++ utils/weatherData.js
@@ -1,22 +1,20 @@
 const request = require('request');
+require('dotenv').config();
 
 const openWeatherMap = {
     BASE_URL: "http://api.openweathermap.org/data/2.5/weather?q=",
-    SECRET_KEY: "097ce497ffef218cb5c9819a2f6263fb"
+    SECRET_KEY: process.env.SECRET_KEY
 }
 
 const weatherData = (address, callback) => {
     const url = openWeatherMap.BASE_URL + encodeURIComponent(address) + "&APPID=" + openWeatherMap.SECRET_KEY;
-
 
     request({ url, json: true }, (error, data) => {
         if (error) {
             callback(true, "Unable to fetch data, please try again" + error)
         }
         callback(false, data?.body);
-        
     });
 };
-
 
 module.exports = weatherData;
